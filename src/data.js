78c44860995c9ce4a1fd2aefde6b9185be827a23
guyllaume <guyllaume.cardinal@gmail.com>
2017-12-18 15:23:46 -0500

    Better dummy data

--- src/data.js
+++ src/data.js
@@ -1,62 +1,62 @@
 export const data = {
     0: {
         id: 0,
-        title: 'Foo',
-        color: 'blue',
+        title: 'Santa does not exist',
+        color: 'cornsilk',
         author: {
-            name: 'John Doe',
+            name: 'Larry David',
             photo: 'https://www.gravatar.com/avatar/1c07be1016e845de514931477c939307?s=50&r=g&d=mm'
         },
-        summary: 'Foo bar baz buz'
+        summary: 'Ut efficitur tortor eros, sed cursus elit mollis et. Aliquam iaculis magna et leo eleifend fringilla.'
     },
     1: {
         id: 1,
-        title: 'Foo',
-        color: 'blue',
+        title: 'Holidays cause happiness',
+        color: 'aqua',
         author: {
-            name: 'John Doe',
+            name: 'Mark Harmon',
             photo: 'https://www.gravatar.com/avatar/1c07be1016e845de514931477c939307?s=50&r=g&d=mm'
         },
-        summary: 'Foo bar baz buz'
+        summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam efficitur accumsan lacinia. Cras interdum venenatis semper.'
     },
     2: {
         id: 2,
-        title: 'Foo',
-        color: 'blue',
+        title: 'Water is wet',
+        color: 'hotpink',
         author: {
-            name: 'John Doe',
+            name: 'Jessica Alba',
             photo: 'https://www.gravatar.com/avatar/1c07be1016e845de514931477c939307?s=50&r=g&d=mm'
         },
-        summary: 'Foo bar baz buz'
+        summary: 'Quisque lacinia pharetra enim, id elementum leo dapibus vel.'
     },
     3: {
         id: 3,
-        title: 'Foo',
-        color: 'blue',
+        title: 'Dinosaurs invented the wheel',
+        color: 'lime',
         author: {
-            name: 'John Doe',
+            name: 'Kris Humphries',
             photo: 'https://www.gravatar.com/avatar/1c07be1016e845de514931477c939307?s=50&r=g&d=mm'
         },
-        summary: 'Foo bar baz buz'
+        summary: 'Integer eu ex sit amet libero egestas tristique id vel nibh. Praesent sed facilisis elit. Mauris imperdiet convallis diam, vitae blandit elit venenatis sed. Sed efficitur tellus eget elit porta viverra.'
     },
     4: {
         id: 4,
-        title: 'Foo',
-        color: 'blue',
+        title: '82% of stats are made up on the spot',
+        color: 'lightslategrey',
         author: {
-            name: 'John Doe',
+            name: 'Owen Wilson',
             photo: 'https://www.gravatar.com/avatar/1c07be1016e845de514931477c939307?s=50&r=g&d=mm'
         },
-        summary: 'Foo bar baz buz'
+        summary: 'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque at risus ullamcorper, finibus ipsum et, convallis libero.'
     },
     5: {
         id: 5,
-        title: 'Foo',
-        color: 'blue',
+        title: 'The guy doing this talk is awesome 😎',
+        color: 'peachpuff',
         author: {
-            name: 'John Doe',
+            name: 'Chace Crawford',
             photo: 'https://www.gravatar.com/avatar/1c07be1016e845de514931477c939307?s=50&r=g&d=mm'
         },
-        summary: 'Foo bar baz buz'
+        summary: 'ulla sapien ex, dapibus id luctus eget, sollicitudin nec purus. Suspendisse potenti. Suspendisse non cursus lacus.'
     },
 };
